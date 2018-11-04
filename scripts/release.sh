4527bf88568fc171029c813312842fb2eec49efa
Loïc Norgeot <norgeotloic@gmail.com>
2018-11-04 17:53:26 +0100

    Updates the script to create releases

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,12 @@
 #Create the zip archive in the home
 rm ~/BakeMyScan.zip
+rm ~/bakemyscan.xml
 
 #Navigate to the addon directory
 cd ~/.config/blender/2.79/scripts/addons/
 
 #Copy the preferences to the folder
-cp .config/blender/2.79/scripts/presets/interface_theme/baemyscan.xml BakeMyScan/bakemyscan.xml
+cp BakeMyScan/bakemyscan.xml ~/bakemyscan.xml
 
 #Zip the meaningful files
 zip -R ~/BakeMyScan.zip 'BakeMyScan/__init__.py' 'BakeMyScan/src/*.py' 'BakeMyScan/icons/*.png' 'BakeMyScan/scripts_meshlab/*.mlx' 'BakeMyScan/README.md' 'BakeMyScan/LICENSE' 'BakeMyScan/scripts/*.py' 'BakeMyScan/tests/*'
